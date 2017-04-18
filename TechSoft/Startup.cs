e1066992f4c2837d376688280b8e72cd8944ddf7
manoj.elavalli@gmail.com <manoj.elavalli@gmail.com>
2017-04-18 01:41:28 -0500

    Add project files.

---
+++ TechSoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechSoft.Startup))]
+namespace TechSoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
